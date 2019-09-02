2b08bf662445e785f9a04b13b435c99c16d43134
firismanda <ryuka.1111@gmail.com>
2019-09-02 16:33:15 +0700

    Simple-Activity

---
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/SimpleActivity.java
@@ -0,0 +1,45 @@
+package id.ac.polinema.androidlifecycle;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.OutputStream;
+
+public class SimpleActivity extends AppCompatActivity {
+
+    private EditText nameInput;
+    private TextView outputText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void handleSubmit(View view) {
+        String name = nameInput.getText().toString();
+        outputText.setText("Hello " + name);
+    }
+}
